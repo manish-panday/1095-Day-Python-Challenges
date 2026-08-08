bb81c2e39d84726986e6f0ecb69ffcdd724f5a1c
Nepal AI & ML Hub <manishpandayrare98@gmail.com>
2026-08-08 15:52:26 +0545

    Day 9 solution Updated

---
+++ Day_9_2026-08-05/solution.py
@@ -0,0 +1,85 @@
+customer_fullname = input("Enter your full name: ")
+product_price = float(input("Enter product price: "))
+quantity = int(input("Enter quantity that you want: "))
+
+total_price = int(product_price * quantity)
+discount_ammount = 0 
+discount = ""
+
+if total_price >= 10000:
+    discount_amount = int(total_price * 20 / 100)
+    discount = "20%"
+elif total_price >= 5000 and total_price <= 9999:
+    discount_amount = int(total_price * 10 / 100)
+    discount = "10%"  
+elif total_price >= 2000 and total_price <= 4999:
+    discount_amout = int(total_price * 5 / 100)
+    discount = "5%"
+elif total_price > 0 and total_price <= 2000:
+    discount_amount = int(total_price * 2 / 100)
+    discount = "0%"
+
+final_price = int(total_price - discount_amount)
+
+if discount == "20%":
+    print("========== SHOPPING RECEIPT ==========")
+    print()
+    print(f"Customer: {customer_fullname}")
+    print()
+    print(f"Product Price: {product_price}")
+    print(f"Quantity: {quantity}")
+    print()
+    print(f"Total Price: {total_price}")
+    print(f"Discount: {discount}")
+    print(f"Discount Amount: {discount_amount}")
+    print()
+    print(f"Final Price: {final_price}")
+    print()
+    print("======================================")
+elif discount == "10%":
+    print("========== SHOPPING RECEIPT ==========")
+    print()
+    print(f"Customer: {customer_fullname}")
+    print()
+    print(f"Product Price: {product_price}")
+    print(f"Quantity: {quantity}")
+    print()
+    print(f"Total Price: {total_price}")
+    print(f"Discount: {discount}")
+    print(f"Discount Amount: {discount_amount}")
+    print()
+    print(f"Final Price: {final_price}")
+    print()
+    print("======================================")
+elif discount == "5%":
+    print("========== SHOPPING RECEIPT ==========")
+    print()
+    print(f"Customer: {customer_fullname}")
+    print()
+    print(f"Product Price: {product_price}")
+    print(f"Quantity: {quantity}")
+    print()
+    print(f"Total Price: {total_price}")
+    print(f"Discount: {discount}")
+    print(f"Discount Amount: {discount_amount}")
+    print()
+    print(f"Final Price: {final_price}")
+    print()
+    print("======================================")
+else:
+    print("========== SHOPPING RECEIPT ==========")
+    print()
+    print(f"Customer: {customer_fullname}")
+    print()
+    print(f"Product Price: {product_price}")
+    print(f"Quantity: {quantity}")
+    print()
+    print(f"Total Price: {total_price}")
+    print(f"Discount: {discount}")
+    print(f"Discount Amount: {discount_amount}")
+    print()
+    print(f"Final Price: {final_price}")
+    print()
+    print("======================================")
+
+    #DONE!!!!!!!!!!!!
